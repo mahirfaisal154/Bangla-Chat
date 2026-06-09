d0e0f5de083832f66a795f3cb24f84d7deba3e89
mahir154 <mahir154@gmail.com>
2026-06-09 18:05:20 +0600

    Render Correction

--- Server/src/index.js
+++ Server/src/index.js
@@ -9,9 +9,11 @@ import cors from "cors"
 import { app,server,io } from "./lib/socket.js"
 
 import path from "path"
+import { fileURLToPath } from "url"
 
 dotenv.config()
-const ___dirname=path.resolve()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 //const app=express()
 app.use(express.json({ limit: "10mb" }))
 
@@ -25,9 +27,9 @@ app.use(cors({
 app.use("/api/auth",authroutes)
 app.use("/api/messages", messageroutes)
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../client/dist")))
+    app.use(express.static(path.join(__dirname,"../../Client/dist")))
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
+    res.sendFile(path.join(__dirname, "../../Client/dist", "index.html"))
   })
 }
 const PORT=process.env.PORT
